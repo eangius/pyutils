58a9d3c88fb05aa583a5e8b64a04a6f21227bca9
eangius <eliangius@hotmail.com>
2023-12-06 15:21:55 -0500

    custom clean command

--- setup.py
+++ setup.py
@@ -1,13 +1,51 @@
 #!usr/bin/env python
 
+import contextlib
 import pyutils as proj
 import setuptools
+from distutils.cmd import Command
+import shutil
 import sys
+import os
 
 
 # Common & relative values.
 PY_VERSION = sys.version_info
 PROJ_LICENSE = 'MIT'
+
+
+class CleanCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        return
+
+    def finalize_options(self):
+        return
+
+    def run(self):
+        folder = os.path.dirname(os.path.abspath(__file__))
+        for root, dirnames, filenames in os.walk(folder):
+            # ignore any hidden folder or file (.venv*, .git*, .dvc*, ...)
+            if any(part.startswith(".") for part in root.split(os.path.sep)):
+                continue
+
+            # types of build artifacts
+            for f in filenames:
+                with contextlib.suppress(Exception):
+                    if any(f.endswith(file_type) for file_type in {".c", ".so", ".html"}):
+                        full_path = os.path.join(root, f)
+                        print(f"removing: {full_path}")
+                        os.remove(full_path)
+
+            # types of build directories
+            for d in dirnames:
+                with contextlib.suppress(Exception):
+                    if d in {'__pycache__', 'build', 'dist', '.egg-info', '.DS_Store', 'runtime'}:
+                        full_path = os.path.join(root, d)
+                        print(f"removing: {full_path}")
+                        shutil.rmtree(full_path)
+        return
 
 
 setuptools.setup(
@@ -39,4 +77,9 @@ setuptools.setup(
         f"License :: OSI Approved :: {PROJ_LICENSE} License",
         "Topic :: Library",
     ],
+
+    # custom commands
+    cmdclass={
+        'clean': CleanCommand,
+    },
 )
